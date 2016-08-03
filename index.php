8265e71462a57156487d3bcbb480fab6d8a55372
hisyam44 <mohammadhisyam4@gmail.com>
2016-08-03 17:00:59 +0700

    Kesel Latihan...

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
-	include "Classes/Database.php";
+	spl_autoload_register(function ($class){
+		include "Classes/".$class.".php";
+	});
 	$database = new Database();
-	$data = [];
-	$data['title'] = "Lorem Ipsum";
-	$data['content'] = "Losad jshd ASFNAS a sfjaksbf aksjfb";
-	$data['excerpt'] = "sad";
-	$data['published_at'] = date("Y-m-d H:i:s");
-	print_r($database->update("articles",$data,"id = 1"));
 ?>
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
